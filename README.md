d8306dceca1af8ac432c84fc3c2435ff243c0e32
Tim Vincent <tim@Tims-MacBook-Pro.local>
2015-10-27 01:15:25 -0400

    Early stage POST/GET

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ChessBackend
+
+##Install
+You should have Python 2.7.X installed (All Mac OS X machines and some Linux machines comes with this pre-installed)
+The flask framework can be installed through Python Package Manager using ```pip install flask```
+
+##Running 
+Start the server with ```python server.py```
+The server will now be bound to port 5000 on your localhost
+
+##Usage
+Currently the only 2 interactive endpoints are /games/<ID>/move and /games/<ID>/status where <ID> is a value 0-99 which is the id of a game
+
+The move endpoint requires a POST request from the client with a JSON object containing the piece name. We use the code color [W|B] with piece first letter [K|Q|k|B|R|P]. Here knight is lowercase k
+The 2nd part of the JSON object is our move to location with the breakdown [row 0-8][col 0-8]
+
+At the status endpoint your browser makes a GET request for the status of the specified board, The returned board is a CSV (on |) layout of that board
+
+##Example 
+To send a move request through your browser ```curl -i -H "Content-Type: application/json" -X POST -d '{"piece":"WK", "moveTo":"14"}' http://localhost:5000/games/1/move```
+
+To see the move you just made go to [http://localhost:5000/games/1/status](http://localhost:5000/games/1/status).
---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+sideLen = 8
+numGames = 100
+games = [[[' ' for _ in range(sideLen)] for _ in range(sideLen)] for _ in range(numGames)] # init 100, 8 x 8 boards
+
+@app.route('/')
+def index():
+	return "Please refer to our API"
+
+@app.route('/games/<int:boardid>/move', methods=['POST'])
+def makeMove(boardid):
+	if not request.json:
+		abort(400)
+
+	move = {
+		'id' : 0,
+		'piece': request.json['piece'],
+		'moveTo': request.json['moveTo']
+	}
+	#.append(move) We should add this object to a list of previous moves
+	#TODO: Sanity check, is move valid
+	row = int(move['moveTo'][0])
+	col = int(move['moveTo'][1])
+	games[boardid][row][col] = move['piece']
+	return jsonify({'move': move}), 201 #Return JSON move followed by OK 
+
+@app.route('/games/<int:boardid>/status', methods=['GET'])
+def boardStatus(boardid):
+	if boardid < 0  or boardid > 99:
+		return "404 - Please enter a board number between 0 and 99"
+	
+	#Stringify the board and return it
+	boardString = ''
+	for r in range(0,sideLen):
+		boardString += '|'
+		for c in range(0,sideLen):
+			boardString += '%s |' % (games[boardid][r][c])
+		boardString += '<br/>'
+ 
+	return boardString
+
+if __name__ == '__main__':
+    app.run(debug=True)
